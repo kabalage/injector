d95cb14fd24c48f5725d81d74997543ef4beab6b
Balázs Kaufmann <keatbloom@gmail.com>
2019-10-19 17:28:29 +0200

    add VueServicePlugin

---
+++ VueServicePlugin.js
@@ -0,0 +1,22 @@
+import createInjector from './index';
+
+export default {
+  install(Vue, options) {
+    Vue.$services = createInjector({
+      setDepsOnWindow: process.env.NODE_ENV === 'development'
+    });
+    Vue.prototype.$services = Vue.$services;
+
+    Vue.mixin({
+      beforeCreate() {
+        if (this.$options.provideServices) {
+          const providers = this.$options.provideServices();
+          this.$services.provide(providers);
+        }
+        if (this.$options.services) {
+          Object.assign(this, this.$services.get(this.$options.services));
+        }
+      }
+    });
+  }
+};
